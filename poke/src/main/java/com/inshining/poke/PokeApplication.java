f4a1209676b596be86b0f865e667798569d20461
inshining <inshining.kim@gmail.com>
2023-10-05 19:47:09 +0900

    feat: project setting

---
+++ poke/src/main/java/com/inshining/poke/PokeApplication.java
@@ -0,0 +1,13 @@
+package com.inshining.poke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeApplication.class, args);
+	}
+
+}
---
+++ poke/src/test/java/com/inshining/poke/PokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inshining.poke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
