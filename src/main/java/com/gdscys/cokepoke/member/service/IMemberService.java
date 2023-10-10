b76e369b1e4ec10105afc479bc36d8cef61e57c5
becooq81 <becooq81@gmail.com>
2023-10-11 00:19:46 +0900

    feat: member service interface

--- src/main/java/com/gdscys/cokepoke/member/service/IMemberService.java
+++ src/main/java/com/gdscys/cokepoke/member/service/IMemberService.java
@@ -1,2 +1,13 @@
-package com.gdscys.cokepoke.member.service;public class IMemberService {
+package com.gdscys.cokepoke.member.service;
+
+import com.gdscys.cokepoke.member.domain.Member;
+import com.gdscys.cokepoke.member.dto.UpdateMemberRequest;
+
+import java.util.List;
+
+public interface IMemberService {
+    Member saveMember(String email, String username, String password);
+    void updateMember(Member member, UpdateMemberRequest request);
+    void deleteMember(Member member);
+    List<Member> findAll(int page);
 }
