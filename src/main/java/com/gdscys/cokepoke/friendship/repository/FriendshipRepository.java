586f77c37e67da55cb323c246674aa13d7349df7
becooq81 <becooq81@gmail.com>
2023-10-11 02:19:48 +0900

    feat: friendship repository

--- src/main/java/com/gdscys/cokepoke/friendship/repository/FriendshipRepository.java
+++ src/main/java/com/gdscys/cokepoke/friendship/repository/FriendshipRepository.java
@@ -1,2 +1,19 @@
-package com.gdscys.cokepoke.friendship.repository;public interface FriendshipRepository {
+package com.gdscys.cokepoke.friendship.repository;
+
+import com.gdscys.cokepoke.friendship.domain.Friendship;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FriendshipRepository extends JpaRepository<Friendship, Long> {
+
+    Page<Friendship> findAllByRequestMember(Long requestMember, Pageable pageable);
+    Page<Friendship> findAllByRecipientMember(Long recipientMember, Pageable pageable);
+    Optional<Friendship> findByRequestMemberAndRecipientMember(Long requestMember, Long recipientMember);
+    Optional<Friendship> findByRequestMemberOrRecipientMemberAndAccepted(Long requestMember, Long recipientMember, boolean accepted);
+
 }
