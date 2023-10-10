944fbdf985b8eb5324a75fca5b784f0042697b2b
becooq81 <becooq81@gmail.com>
2023-10-11 02:16:00 +0900

    feat: friendship

---
+++ src/main/java/com/gdscys/cokepoke/member/domain/Friendship.java
@@ -0,0 +1,37 @@
+package com.gdscys.cokepoke.member.domain;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity(name = "Friends")
+@Getter
+@SequenceGenerator(name = "FRIENDS_SEQ_GEN", sequenceName = "FRIENDS_SEQ", allocationSize = 1)
+public class Friendship {
+
+    @Id @GeneratedValue(generator = "FRIENDS_SEQ_GEN")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Member requestMember;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id")
+    private Member recipientMember;
+
+    @Column(name = "is_accepted", columnDefinition = "boolean default false")
+    private boolean isAccepted;
+
+    protected Friendship() {}
+
+    public Friendship(Member requestMember, Member recipientMember) {
+        this.requestMember = requestMember;
+        this.recipientMember = recipientMember;
+    }
+
+    public void accept() {
+        this.isAccepted = true;
+    }
+
+}
