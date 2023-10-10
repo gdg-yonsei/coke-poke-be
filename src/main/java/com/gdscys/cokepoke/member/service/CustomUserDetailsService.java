840f8ff621b885f9664866114acac60aa7a9bf46
becooq81 <becooq81@gmail.com>
2023-10-11 00:16:25 +0900

    feat: custom user details service

--- src/main/java/com/gdscys/cokepoke/member/service/CustomUserDetailsService.java
+++ src/main/java/com/gdscys/cokepoke/member/service/CustomUserDetailsService.java
@@ -1,2 +1,30 @@
-package com.gdscys.cokepoke.member.service;public class CustomUserDetailsService {
+package com.gdscys.cokepoke.member.service;
+
+import com.gdscys.cokepoke.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return memberRepository.findByUsername(username)
+                .map(member -> User.builder()
+                        .username(member.getUsername())
+                        .password(member.getPasswordHash())
+                        .roles(Arrays.toString(member.getRoles().toArray()))
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
 }
