2e4aae7eba58cd4715f310c4a6b4040abdac2bf2
becooq81 <becooq81@gmail.com>
2023-10-11 00:13:23 +0900

    feat: login request

--- src/main/java/com/gdscys/cokepoke/auth/dto/LoginRequest.java
+++ src/main/java/com/gdscys/cokepoke/auth/dto/LoginRequest.java
@@ -1,2 +1,16 @@
-package com.gdscys.cokepoke.auth.dto;public class LoginRequest {
+package com.gdscys.cokepoke.auth.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+    private String email;
+    private String password;
+
+    protected LoginRequest() {}
+
+    public LoginRequest(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
