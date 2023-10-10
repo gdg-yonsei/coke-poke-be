bcba2789db4bd870261db3ca22697e43f389d689
becooq81 <becooq81@gmail.com>
2023-10-11 02:39:47 +0900

    feat: friendship request

--- src/main/java/com/gdscys/cokepoke/friendship/dto/FriendshipRequest.java
+++ src/main/java/com/gdscys/cokepoke/friendship/dto/FriendshipRequest.java
@@ -1,2 +1,13 @@
-package com.gdscys.cokepoke.friendship.dto;public class FriendshipRequest {
+package com.gdscys.cokepoke.friendship.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FriendshipRequest {
+    private String requestUsername;
+    private String recipientUsername;
+
+    protected FriendshipRequest() {}
 }
