3570a34afe8bd0ed122b9997261f1a108cdccbd8
Sangwon Park <sangwonpark@Sangwons-MacBook-Pro.local>
2023-10-11 14:31:03 +0900

    feat: project settings

---
+++ coke-poke/src/main/java/com/hobbychive/cokepoke/CokePokeApplication.java
@@ -0,0 +1,13 @@
+package com.hobbychive.cokepoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokePokeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CokePokeApplication.class, args);
+	}
+
+}
