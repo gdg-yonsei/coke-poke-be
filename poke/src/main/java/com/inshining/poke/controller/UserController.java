bfe95c4da6a0f3112c7e78f8eefc81b120c1d53b
inshining <inshining.kim@gmail.com>
2023-10-11 18:02:56 +0900

    fix:  로그인 구현을 확인하기 위한 임시 엔드포인트

--- poke/src/main/java/com/inshining/poke/controller/UserController.java
+++ poke/src/main/java/com/inshining/poke/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.inshining.poke.controller;
 
+import com.inshining.poke.domain.dto.SignInRequest;
+import com.inshining.poke.domain.dto.SignInResponse;
 import com.inshining.poke.domain.dto.SignUpRequest;
 import com.inshining.poke.domain.dto.SignUpResponse;
 import com.inshining.poke.domain.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,4 +21,15 @@ public class UserController {
     public SignUpResponse signUp(@RequestBody SignUpRequest request){
         return service.registerUser(request);
     }
+
+    @PostMapping("/login")
+    public SignInResponse signIn(@RequestBody SignInRequest request) {
+        return service.signIn(request);
+    }
+
+    @GetMapping("/my")
+    public String getMyPage(){
+        return "success";
+    }
+
 }
