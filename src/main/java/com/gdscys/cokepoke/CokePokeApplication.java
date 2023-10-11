9f8f91cc2ff5bb1de9b15f23c97006403af7359a
becooq81 <becooq81@gmail.com>
2023-10-11 20:43:52 +0900

    changes

---
+++ src/main/java/com/gdscys/cokepoke/CokePokeApplication.java
@@ -0,0 +1,13 @@
+package com.gdscys.cokepoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CokePokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CokePokeApplication.class, args);
+    }
+
+}
