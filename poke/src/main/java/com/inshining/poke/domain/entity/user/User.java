02d7e53e971b54ea7c32ad0f4da22217951dd46d
inshining <inshining.kim@gmail.com>
2023-10-11 21:45:18 +0900

    feat: 유저 아이디 유니크 처리

--- poke/src/main/java/com/inshining/poke/domain/entity/user/User.java
+++ poke/src/main/java/com/inshining/poke/domain/entity/user/User.java
@@ -23,7 +23,7 @@ public class User extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(unique = true)
     private String username;
 
     @Column(nullable = false)
