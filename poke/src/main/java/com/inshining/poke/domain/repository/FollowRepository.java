b96241a7ff38eafef8049e8fb1aa34af9bb45f7c
inshining <inshining.kim@gmail.com>
2023-11-28 16:53:11 +0900

    feat: Follow repository 생성

---
+++ poke/src/main/java/com/inshining/poke/domain/repository/FollowRepository.java
@@ -0,0 +1,11 @@
+package com.inshining.poke.domain.repository;
+
+import com.inshining.poke.domain.entity.follow.Follow;
+import com.inshining.poke.domain.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FollowRepository extends JpaRepository<Follow, Long> {
+    List<User> findFollowingUsersByFollowerUser(User FollowerUser);
+}
