6c14f129e7985dc95ae5a47c47a4500cd104279f
inshining <inshining.kim@gmail.com>
2023-11-28 23:31:49 +0900

    feat: follow repository method 추가

--- poke/src/main/java/com/inshining/poke/domain/repository/FollowRepository.java
+++ poke/src/main/java/com/inshining/poke/domain/repository/FollowRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FollowRepository extends JpaRepository<Follow, Long> {
-    List<User> findFollowingUsersByFollowerUser(User FollowerUser);
+    List<Follow> findAllByFollowerUser(User followerUser);
+    List<Follow> findAllByFollowingUser(User followerUser);
 }
